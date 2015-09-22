6531f6b5fee5b71d0bf0a70c39c87236c877b5ff
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-09-22 14:50:32 +0200

    fixed installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['tactile_marker_publisher','tactile_merger'],
+    packages=['tactile_marker_publisher'],
     package_dir={'': 'src'},
     scripts=['scripts/tactile_marker_publisher','scripts/tactile_merger'],
     requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'visualization_msgs', 'tf', 'numpy', 'webcolors']
